57faf40c0d68200decc7dd285eef95f8f55b4d25
misteltein <itogahibiki@gmail.com>
2022-03-27 12:28:49 +0900

    Added test for genrand_int32

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,9 +1,24 @@
 import MersenneTwister from "../src/index";
 
 const positiveMT = require('./jsons/constructor_mt_seed_123456.json');
+const positive_genrand_int32 = require('./jsons/genrand_int32_seed_123456.json');
 
-test('constructor mt', () => {
+test('mt after construction', () => {
     const mt = new MersenneTwister(123456);
     const sample = mt._preview().mt;
     expect(sample).toEqual(positiveMT.mt);
 });
+
+test('mti after construction', () => {
+    const mt = new MersenneTwister(123456);
+    const sample = mt._preview().mti;
+    expect(sample).toEqual(624);
+});
+
+test('first 10^5 samples of genrand_int32', () => {
+    const mt = new MersenneTwister(123456);
+    positive_genrand_int32.genrand_int32.forEach((r: Number) => {
+        expect(r).toEqual(mt.genrand_int32());
+    })
+});
+
